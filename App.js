b865be3b50122f70222744816ba35a781bfb509b
JadenWPuckett <jwpucket@asu.edu>
2022-12-23 14:01:06 -0700

    Finished first app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+    Notes for running:
+
+        - Expo CLI start metro bundler (HTTP server that compiles javascript using Babel and serves it to Expo app)
+        
+            npx expo start              (OR npm start)
+
+        - Exit
+
+            Ctrl+C
+*/
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import registerNNPushToken from 'native-notify';
+
+import Home from './src/Screens/Home';
+import ChosenTask from './src/Screens/ChosenTask';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    // push notifications
+    registerNNPushToken(5368, '6UNQ6ckAm0sCcbBRw3DUDj');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    // globalstate managmenet
+    const [toDoList, setToDoList] = useState([{ id: 1, task: 'brush your teeth'}]);
+    const [task, setTask] = useState('');
+    const [chosenTask, setChosenTask] = useState('');
+
+    const GlobalState = {
+        toDoList, setToDoList,
+        task, setTask,
+        chosenTask, setChosenTask
+    }
+
+    // navigation
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+
+                <Stack.Screen name="Home" options={{ headerShown: false }}>
+                    {props => <Home {...props} GlobalState = {GlobalState} />}
+                </Stack.Screen>
+
+                <Stack.Screen name="ChosenTask" options={{ headerShown: false }}>
+                    {props => <ChosenTask {...props} GlobalState = {GlobalState} />}
+                </Stack.Screen>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import { StyleSheet, View, Text } from "react-native";
+import Constants from 'expo-constants';
+
+export default function Header() {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>My To Do List</Text> 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        paddingTop: Constants.statusBarHeight,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 6,
+        },
+        shadowOpacity: 0.37,
+        shadowRadius: 7.49,
+        elevation: 12,
+        zIndex: 5
+    },
+    text: {
+        fontSize: 18,
+        fontWight: '900',
+        color: ''
+    }
+})
